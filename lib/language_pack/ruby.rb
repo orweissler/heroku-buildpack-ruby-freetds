10d7b324648097c4b1bc143fa17c6897010fe4d7
Or Weissler <orweissler@gmail.com>
2015-02-01 13:30:19 +0000

    More debugging

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -389,6 +389,7 @@ ERROR
         env_vars       = "env BUNDLE_GEMFILE=#{pwd}/Gemfile BUNDLE_CONFIG=#{pwd}/.bundle/config CPATH=#{yaml_include}:$CPATH CPPATH=#{yaml_include}:$CPPATH LIBRARY_PATH=#{yaml_lib}:$LIBRARY_PATH RUBYOPT=\"#{syck_hack}\""
         puts "Running: #{bundle_command}"
 		puts "Debug 4"
+		puts env_vars
         bundler_output << pipe("#{env_vars} #{bundle_command} --no-clean 2>&1")
 		puts "Debug 5"
       end
